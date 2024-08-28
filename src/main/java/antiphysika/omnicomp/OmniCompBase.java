7791f1f6e68e71cfda0e9dc35396ea3d5f58fef2
Francis <antiphysika@gmail.com>
2024-08-28 03:14:42 +0100

    [Javadoc] Add placeholder comments to class `OmniCompBase`

--- src/main/java/antiphysika/omnicomp/OmniCompBase.java
+++ src/main/java/antiphysika/omnicomp/OmniCompBase.java
@@ -10,16 +10,36 @@ import static antiphysika.omnicomp.OmniComp.MOD_ID;
 
 public class OmniCompBase
 {
+  /**
+   * [JAVADOC-PLACEHOLDER]
+   *
+   * @param path
+   * @return
+   */
   public static ResourceLocation location (String path)
   {
     return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
   }
 
+  /**
+   * [JAVADOC-PLACEHOLDER]
+   *
+   * @param namespace
+   * @param path
+   * @return
+   */
   public static ResourceLocation location (String namespace, String path)
   {
     return ResourceLocation.fromNamespaceAndPath(namespace, path);
   }
 
+  /**
+   * [JAVADOC-PLACEHOLDER]
+   *
+   * @param id
+   * @param level
+   * @return
+   */
   public static ResourceLocation blockLocation (String id, int level)
   {
     return location(String.format("%s_%dx", id, level));
